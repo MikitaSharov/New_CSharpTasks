0fcc7e6932b4be97c6b8d21a9b15989b6a715f1d
Nikita Sharov <GB@gmail.com>
2023-08-07 19:47:44 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,9 @@
+﻿// Известны два расстояния: одно в километрах, другое — в футах (1 фут 0,305 м). Какое из расстояний меньше?
+
+int km = new Random().Next(0,100);
+int ft = new Random().Next(0,100000);
+double ftToKm = ft * 0.000305;
+
+
+if (km < ftToKm) Console.WriteLine($"{km} км. меньше чем {ft} футов");
+else Console.WriteLine($"{ft} футов. меньше чем {km} км");
