14693299934a8345b651ea6a65085c9221acf89a
Nikita Sharov <lliapik@gmail.com>
2023-08-07 13:58:39 +0300

    task001_RandomArray

---
+++ task001/Program.cs
@@ -0,0 +1,14 @@
+﻿/* написать метод заполнения массива рандомными цифрами от 10 до 99
+(этот метод нам дальше будет нужен везде, так что делаем красивый. чтобы цифры через ",", */
+
+int[] array = new int[10];
+
+void RandomArray(int[] collection)
+{
+    int length = collection.Length;
+
+    for (int index = 0; index < length; index++) collection[index] = new Random().Next(10, 100);
+    foreach (int item in array) Console.Write($"{item}, ");
+}
+
+RandomArray(array);
