9d7ee694e6b8c7afcad7aeb0e6a877afe46591db
Nikita Sharov <GB@gmail.com>
2023-08-07 19:19:53 +0300

    string.Join

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,14 +1,15 @@
 ﻿/* написать метод заполнения массива рандомными цифрами от 10 до 99
 (этот метод нам дальше будет нужен везде, так что делаем красивый. чтобы цифры через ",", */
 
-int[] array = new int[10];
+int[] array = new int[10]; // размер массива
 
 void RandomArray(int[] collection)
 {
     int length = collection.Length;
 
     for (int index = 0; index < length; index++) collection[index] = new Random().Next(10, 100);
-    foreach (int item in array) Console.Write($"{item}, ");
+   /*  foreach (int item in array)  */
+    Console.Write(string.Join(", ", array));
 }
 
 RandomArray(array);
