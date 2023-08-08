b34e3afa929546d3d0a76645df198a393c67b67a
Nikita Sharov <GB@gmail.com>
2023-08-08 20:50:48 +0300

    task007

---
+++ task007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Проверить, принадлежит ли число, введенное с клавиатуры, интервалу (–5, 3).
+
+Console.Write("Enter numder: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (number<3 && number>-5) Console.Write("true");
+else Console.Write("false");
