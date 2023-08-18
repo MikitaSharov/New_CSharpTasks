00e5ff827b7ea0e80e3b2b2f944f656c693a5c4c
Nikita Sharov <lliapik@gmail.com>
2023-08-18 10:28:46 +0300

    task013

---
+++ task013/Program.cs
@@ -0,0 +1,11 @@
+﻿/* В чемпионате по футболу команде за выигрыш дается 3 очка, за проигрыш — 0, за ничью — 1.
+Известно количество очков, полученных командой за игру.
+Определить словесный результат игры (выигрыш, проигрыш или ничья). */
+
+Console.Write("Введите очки: ");
+int points = Convert.ToInt16(Console.ReadLine());
+
+if (points == 3) Console.Write("победа");
+else if (points == 1) Console.Write("ничья");
+else if (points == 0) Console.Write("проигрыш");
+else Console.Write("неверный ввод");
