54d0cac782f85ee59032bc82f5db7f3c2899faba
Nikita Sharov <lliapik@gmail.com>
2023-08-18 10:10:56 +0300

    CountEven for int numbers

---
+++ task012/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны четыре целых числа. Определить, сколько из них четных.
+
+int countEven = 0;
+countEven = CountEven(6, 2, 3, 4);
+int CountEven(int number1, int number2, int number3, int number4)
+{
+    if (number1 % 2 == 0) countEven++;
+    if (number2 % 2 == 0) countEven++;
+    if (number3 % 2 == 0) countEven++;
+    if (number4 % 2 == 0) countEven++;
+
+    return countEven;
+}
+
+Console.Write(countEven);
