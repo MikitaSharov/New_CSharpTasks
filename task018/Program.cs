bf4f1de1e25ab51d1a585d67a6fc9551df1130f5
Nikita Sharov <lliapik@gmail.com>
2023-08-21 11:24:39 +0300

    окончания

---
+++ task018/Program.cs
@@ -0,0 +1,9 @@
+﻿// Для натурального числа k напечатать фразу «мы нашли k грибов в лесу», согласовав окончание слова «гриб» с числом k.
+
+Console.WriteLine("How much mushrooms?");
+int qty = Convert.ToInt16(Console.ReadLine());
+
+if (qty == 1) Console.WriteLine($"Мы нашли {qty} гриб в лесу");
+else if (qty > 1 && qty < 5) Console.WriteLine($"Мы нашли {qty} гриба в лесу");
+else if (qty > 4) Console.WriteLine($"Мы нашли {qty} грибов в лесу");
+else Console.WriteLine($"Мы нашли не нашли грибов в лесу");
